d6170cb9ae70fe89f0a5e039e002eb65243b3225
SpiffyJohnson <135999968+SpiffyJohnson@users.noreply.github.com>
2024-04-27 19:14:41 -0500

    Added the Scale function
    Added the scale function and input popup.

--- main.py
+++ main.py
@@ -21,9 +21,11 @@ import cv2
 
 global CurrentImagePath # The path of the original entered image
 global RealImage # The actual image, which will be modified while retaining its original size.
+global InputUp # Whether an input prompt is already up. Avoids stacking windows.
 
 current_image = None
 CurrentImagePath = None
+InputUp = False
 
 # "Dark mode" Palette:
 
@@ -34,24 +36,6 @@ MidDarkest = "#111111"
 Darkest = "#141414"
 ButtonColor = MidLightest
 DescriptionTextColor = "#FFF"
-
-# Salmon palette (requires label color swapping)
-
-#Highlight = "#8E8D8A"
-#Lightest = "#EAE7DC"
-#MidLightest = "#D8C3A5"
-#MidDarkest = "#E98074"
-#Darkest = "#E85A4F"
-#ButtonColor = "#D8C3A5"
-
-# Original grayscale color scheme:
-
-#Highlight = "#DFDFDF"
-#Lightest = "#898989"
-#MidLightest = "#5C5C5C"
-#MidDarkest = "#393939"
-#Darkest = "#000000"
-#ButtonColor = Lightest
 
 # Button design:
 
@@ -248,10 +232,82 @@ def convert_to_flip_v():
         MainImage.image = photo
 
 def convert_to_scaled():
-    global current_image, MainImage, RealImage
+    global current_image, MainImage, RealImage, InputUp
 
     if current_image is not None:
-        pass
+        # Attempts to scale the image by the selected value.
+        def submit():
+            global RealImage, current_image, MainImage, InputUp
+
+            # Test if the image will be too big after resizing.
+            height, width, _ = RealImage.shape
+            if (height * value > 3840 or width * value > 3840):
+                value_label.config(text="Image must be smaller than 3840 pixels!")
+            elif (height * value < 1 or width * value < 1):
+                value_label.config(text="Image must be larger than 0 pixels!")
+            else:
+                # Resize both images with the factor provided by the prompt.
+                RealImage = cv2.resize(RealImage, None, fx=value, fy=value, interpolation=cv2.INTER_NEAREST)
+                current_image = cv2.resize(current_image, None, fx=value, fy=value, interpolation=cv2.INTER_NEAREST)
+                current_image = Resizer(current_image)
+                binary_image = cv2.cvtColor(current_image, cv2.COLOR_BGR2RGB)
+
+                pil_img = Image.fromarray(binary_image)
+                photo = ImageTk.PhotoImage(pil_img)
+                MainImage.config(image=photo)
+                MainImage.image = photo
+                UpdateSizeDisplay(RealImage)
+                InputUp = False
+                popup.destroy()
+
+        # Gets the current value of the input slider and updates the display label.
+        def update_label(event):
+            global image_size_label, value
+
+            height, width, _ = RealImage.shape
+            value = slider.get()
+            value_label.config(text=f"Scale to: {round(value * 100)}%")
+            if (round(width * value, 1) < 1) or round(height * value, 1) < 1:
+                image_size_label.config(text=(str(round(width * value, 1)) + " x " + str(round(height * value, 1))))
+            else:
+                image_size_label.config(text=(str(round(width * value)) + " x " + str(round(height * value))))
+
+        # Creates the graphical popup.
+        def CreatePopup():
+            global slider, popup, value_label, RealImage, image_size_label, InputUp
+            # Tell the window to not allow the program to open a new prompt while the old one is open.
+            InputUp = True
+
+            # Tell the window to allow the program to open a new prompt upon closing.
+            def UpdateInputUp():
+                global InputUp
+                InputUp = False
+                popup.destroy()
+            
+            popup = tk.Toplevel(bg=MidLightest)
+            popup.title("Scale Value")
+            popup.protocol("WM_DELETE_WINDOW", UpdateInputUp)
+                
+            slider = tk.Scale(popup, bg=MidDarkest, fg=DescriptionTextColor, background=MidLightest, troughcolor=Darkest,
+                            from_=0.1, to=2, digits=2, highlightthickness=0, borderwidth=0, length=200, width=20, 
+                            orient="horizontal", resolution=0.1, command=update_label)
+            slider.set(1)
+            slider.pack(padx=20, pady=10)
+                
+            value_label = tk.Label(popup, bg=MidLightest, fg=DescriptionTextColor, text="Selected value: 0")
+            value_label.pack(pady=5)
+
+            # Show the projected height and width of the resized image.
+            height, width, _ = RealImage.shape
+            image_size_label = tk.Label(popup, bg=MidLightest, fg=DescriptionTextColor, text=(str(round(height)) + " x " + str(round(width))))
+            image_size_label.pack(pady=5)
+                
+            submit_button = tk.Button(popup, bg=ButtonColor, fg=DescriptionTextColor, text="Resize", command=submit)
+            submit_button.pack(pady=5)
+            
+        # Calls the function to open the input popup.
+        if (not InputUp):
+            CreatePopup()
 
 def convert_to_inverted():
     global current_image, MainImage, RealImage
@@ -311,6 +367,30 @@ def convert_to_randomized():
         photo = ImageTk.PhotoImage(pil_img)
         MainImage.config(image=photo)
         MainImage.image = photo
+
+def convert_to_randomized_EE():
+
+    b, g, r, a = cv2.split(cv2.imread((IMAGE_PATH + "Logo.png"), cv2.IMREAD_UNCHANGED))
+    random_b = np.random.randint(0, 256)
+    random_g = np.random.randint(0, 256)
+    random_r = np.random.randint(0, 256)
+
+    # Apply the random colors only to non-transparent pixels using a mask
+    mask = a > 0
+    b[mask] = np.clip(b[mask] + random_b, 0, 255).astype(np.uint8)
+    g[mask] = np.clip(g[mask] + random_g, 0, 255).astype(np.uint8)
+    r[mask] = np.clip(r[mask] + random_r, 0, 255).astype(np.uint8)
+
+    inverted_image = cv2.merge((b, g, r, a))
+    inverted_image = cv2.cvtColor(inverted_image, cv2.COLOR_BGR2RGBA)
+
+    pil_img = Image.fromarray(inverted_image)
+    photo = ImageTk.PhotoImage(pil_img)
+    HeaderImage.config(image=photo)
+    HeaderImage.image = photo
+
+def HeaderEE():
+    HeaderText.config(fg=("#" + os.urandom(6).hex()))
 
 def GetInput(DotNumber, Direction):
     global RealImage
@@ -431,7 +511,7 @@ FHorizButton = tk.Button(BottomFrame, image=FlipHButtonImage, bg=ButtonColor, re
 FHorizButton.pack(fill="both", expand=True, side="left")
 FVertiButton = tk.Button(BottomFrame, image=FlipVButtonImage, bg=ButtonColor, relief=BorderStyle, bd=BorderWidth, fg=TextColor, font=FONT, command=convert_to_flip_v)
 FVertiButton.pack(fill="both", expand=True, side="left")
-ResizeButton = tk.Button(BottomFrame, image=ResizeImage, bg=ButtonColor, relief=BorderStyle, bd=BorderWidth, fg=TextColor, font=FONT)
+ResizeButton = tk.Button(BottomFrame, image=ResizeImage, bg=ButtonColor, relief=BorderStyle, bd=BorderWidth, fg=TextColor, font=FONT, command=convert_to_scaled)
 ResizeButton.pack(fill="both", expand=True, side="left")
 InvertButton = tk.Button(BottomFrame, image=InvertImage, bg=ButtonColor, relief=BorderStyle, bd=BorderWidth, fg=TextColor, font=FONT, command=convert_to_inverted)
 InvertButton.pack(fill="both", expand=True, side="left")
@@ -442,7 +522,7 @@ RotateButton.pack(fill="both", expand=True, side="left")
 ResetButton = tk.Button(BottomFrame, image=UndoImage, bg=ButtonColor, relief=BorderStyle, bd=BorderWidth, fg=TextColor, font=FONT, command=reset_image)
 ResetButton.pack(fill="both", expand=True, side="left")
 
-Separator2 = tk.Label(BottomFrame, text="          ", bg=Lightest) # Makes space on the bottom row
+Separator2 = tk.Button(BottomFrame, text="          ", activebackground=MidLightest, borderwidth=0, bg=Lightest, command=HeaderEE) # Makes space on the bottom row
 Separator2.pack(fill="both", expand=True, side="left")
 
 # Bottom Text Descriptions and padding:
@@ -469,7 +549,7 @@ RotateDescription.pack(fill="both", expand=True, side="left")
 UndoDescription = tk.Label(BottomDescriptions, text="  Reset Image    ", bg=Lightest, fg=DescriptionTextColor)
 UndoDescription.pack(fill="both", expand=True, side="left")
 
-Separator4 = tk.Label(BottomDescriptions, text="          ", bg=Lightest)
+Separator4 = tk.Button(BottomDescriptions, text="          ", relief=None, borderwidth=0, activebackground=MidLightest, bg=Lightest, command=convert_to_randomized_EE)
 Separator4.pack(fill="both", expand=True, side="left")
 
 root.mainloop()
